df8bb42467789c05143304fd7b66fb81f346f733
Rondre Diederich <rdiederich19@gmail.com>
2026-07-23 10:24:16 -0600

    responsive layout & sidebar mobile menu added

--- script.js
+++ script.js
@@ -1,73 +1,38 @@
 // Navigation Menu Toggle
 document.addEventListener('DOMContentLoaded', function () {
-        const navbarToggle = document.getElementById('navbarToggle');
-        const navbarMenu = document.getElementById('navbarMenu');
-        if (navbarToggle) {
-            navbarToggle.addEventListener('click', function() {
-                navbarToggle.classList.toggle('is-active');
-                navbarMenu.classList.toggle('is-active');
-            });
+        function w3_open() {
+            document.getElementById("mySidebar").style.width = "100%";
+            document.getElementById("mySidebar").style.display = "block"
         }
-        const megaMenuToggles = document.querySelectorAll('.mega-menu-toggle');
-        megaMenuToggles.forEach(toggle => {
-            toggle.addEventListener('click', function(event) {
-                const isMobileView = getComputedStyle(navbarToggle).display !== 'none';
-                if (isMobileView) {
-                    event.preventDefault();
-                    const parentItem = this.closest('.mega-menu-item');
-                    parentItem.classList.toggle('is-active');
-                }
-            });
+
+        function w3_close() {
+            document.getElementById("mySidebar").style.display = "none"
+        }
+
+        const hamburger = document.getElementById("hamburger");
+        const mobile_links = document.getElementById("mySidebar");
+
+        hamburger.addEventListener('click', function() {
+            document.getElementById("mySidebar").style.width = "100%";
+            document.getElementById("mySidebar").style.display = "block"
+            document.getElementById("close-btn").style.display = "block"
+            hamburger.style.display = "none";
+            document.getElementById("site-header").style.justifyContent = "space-between"
+            
+            mobile_links.style.color = "var(--blue-dark)"
+
+        })
+
+        const close = document.getElementById("close-btn");
+
+        close.addEventListener('click', function () {
+            console.log("close button click event");
+            mobile_links.style.display = "none";
+            hamburger.style.display = "block";
+            close.style.display = "none";
         });
+        
 
-
-        // Flights & Hotels Tab
-        const flightsBtn = document.getElementById('flights');
-        const hotelsBtn = document.getElementById('hotels');
-
-        function handleFlightBtn() {
-            flightsBtn.classList.add('active');
-            hotelsBtn.classList.remove('active')
-        }
-
-        function handleHotelsBtn() {
-            hotelsBtn.classList
-        }
+        
+        
 });
-
-// slideshow
-let slideIndex = 1;
-showSlides(slideIndex);
-// next, prev
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-    showSlides(slideIndex) = n
-};
-function showSlides (n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-
-    if (n < 1) {
-        slideIndex = slides.length;
-    };
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "")
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += "active";
-}
-
-
